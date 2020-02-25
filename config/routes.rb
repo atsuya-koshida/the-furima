02be44a06754d14475d791baacee7c0b8d6e5b8b
9eko <remonskash@gmail.com>
2020-02-25 20:47:31 +0900

    商品購入確認画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root "items#home"
-  resources :items, only: [:new, :show]
+  resources :items, only: [:new, :show] do
+    collection do
+      get 'purchase', to: 'items#purchase'
+    end
+  end
 end
