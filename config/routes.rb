7ad884f662123bf5ff65f1878159ad82bdc3bfe4
shuntaro <shun.sea.f@gmail.com>
2020-02-26 12:22:23 +0900

    カテゴリー一覧ページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "items#home"
   resources :items, only: [:new, :show]
+  get 'categories/itemsindex', to: 'categories#itemsindex'
+  resources :categories, only: [:index]
 end
