5854e578884ca693452c3e5537b1f79cc147da85
atsuyakoshida <koshi0845@gmail.com>
2020-02-26 14:48:45 +0900

    fix payjp.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require activestorage
 //= require rails-ujs
+//= require activestorage
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,7 +1,7 @@
 document.addEventListener(
   "DOMContentLoaded", e => {
     if (document.getElementById("token_submit") != null) { //token_submitというidがnullの場合、下記コードを実行しない
-      Payjp.setPublicKey("sk_test_b8dea0a3f906e281ab7fb0e5"); //ここに公開鍵を直書き
+      Payjp.setPublicKey("pk_test_e3952bde8168a67bd0febe06"); //ここに公開鍵を直書き
       let btn = document.getElementById("token_submit"); //IDがtoken_submitの場合に取得されます
       btn.addEventListener("click", e => { //ボタンが押されたときに作動します
         e.preventDefault(); //ボタンを一旦無効化します
