8e25b9dc5f0eca24e89b72b9c3a7c824dac1c34a
9eko <remonskash@gmail.com>
2020-02-26 14:31:31 +0900

    商品出品機能の定義
    edit
    ・routes.rb
    ・items_controller.rb
    ・items/new.html.hsml

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,5 +6,30 @@ class ItemsController < ApplicationController
   def purchase
   end
 
+  def new
+    @item = Item.new
+    @item.images.new
+    @parents = Category.where(ancestry: nil)
+  end
+
+  def create
+    # binding.pry
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      @parents = Category.where(ancestry: nil)
+      @item = Item.new(item_params)
+      @item.images.new
+      render :new
+      # flash.now[:alert] = '出品に失敗しました'
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :condition_id, :postage_id, :prefecture_id, :schedule_id, :price, :category_id, images_attributes: [:image]).merge(user_id: current_user.id)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "items#home"
-  resources :items, only: [:new, :show, :edit] do
+  resources :items, only: [:new, :create, :show, :edit] do
     collection do
       get 'purchase', to: 'items#purchase'
     end
