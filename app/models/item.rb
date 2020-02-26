f247ad2d417aff9c67d41ee71b2b836bdf73b6ba
9eko <remonskash@gmail.com>
2020-02-26 14:09:33 +0900

    imageの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,6 @@ class Item < ApplicationRecord
   belongs_to_active_hash :postage
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :schedule
+  has_many :images, dependent: :destroy
+  accepts_nested_attributes_for :images
 end
