8ebed9f6ec30e0accd0cf2ad4b24051c8ab9318a
9eko <remonskash@gmail.com>
2020-02-26 13:40:08 +0900

    active_hashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :schedule
 end
