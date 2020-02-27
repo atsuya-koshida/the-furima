cf5ca4f0152179b79e0f1d05b6a4296afc0cd01a
atsuya-koshida <59789801+atsuya-koshida@users.noreply.github.com>
2020-02-27 11:07:47 +0900

    Payjpを用いた商品購入機能の実装・購入確認ページと購入完了ページのマークアップ (#15)
    * add payjp gem
    
    * rails g model card
    
    * rails g controller card
    
    * add jquery-rails
    
    * jquery-rails 設定
    
    * add payjp.js
    
    * add routes card
    
    * fix payjp
    
    * card views changes
    
    * fix payjp.js
    
    * delete comments
    
    * fix show.html.haml
    
    * fix card controller
    
    * routes fix
    
    * card#new fix
    
    * routes fix
    
    * fix set_card
    
    * card controller fix
    
    * routes fix
    
    * add purchse function
    
    * fix items#show view
    
    * change views items#done#purchase
    
    * fix items controller
    
    * fix items#show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,16 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :purchase, :pay, :done]
+  before_action :set_card, only: [:purchase, :pay, :done]
+  before_action :authenticate_user!, only: [:purchase, :pay, :done]
 
   def home
   end
 
-  def purchase
+  def show
+    @user = User.find(@item.user_id)
+    @category = Category.find(@item.category_id)
   end
-
+  
   def new
     @item = Item.new
     @item.images.new
@@ -35,10 +40,43 @@ class ItemsController < ApplicationController
     end
   end
 
+  def purchase
+    if @card.blank?
+      redirect_to controller: "card", action: "new"
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
+    end
+  end
+
+  def pay
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    Payjp::Charge.create(
+    :amount => @item.price,
+    :customer => @card.customer_id,
+    :currency => 'jpy'
+    )
+    Item.update(bought_user_id: current_user.id)
+    redirect_to action: 'done' 
+  end
+
+  def done
+    @user = User.find_by(id: @item.user_id)
+    @category = Category.find_by(id: @item.category_id)
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:title, :description, :condition_id, :postage_id, :prefecture_id, :schedule_id, :price, :category_id, images_attributes: [:image]).merge(user_id: current_user.id)
   end
 
+  def set_card
+    @card = Card.find_by(user_id: current_user)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
