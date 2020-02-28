9bc530a662f4a21d5141f4c1855c28fb6517189c
atsuyakoshida <koshi0845@gmail.com>
2020-02-28 18:15:04 +0900

    add card test

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,10 +1,23 @@
 require 'rails_helper'
-describe Card do
-  describe '#pay' do
-    it "is invalid without a card_id" do
-      card = build(:card, card_id: "")
-      card.valid?
-      expect(card.errors[:card_id]).to include("can't be blank")
+
+RSpec.describe Card, type: :model do
+  describe Card do
+    describe '#pay' do
+      it "is valid with all" do
+        expect(build(:card)).to be_valid
+      end
+
+      it "is invalid without a card_id" do
+        card = build(:card, card_id: "")
+        card.valid?
+        expect(card.errors[:card_id]).to include("can't be blank")
+      end
+
+      it "is invalid without a customer_id" do
+        card = build(:card, customer_id: "")
+        card.valid?
+        expect(card.errors[:customer_id]).to include("can't be blank")
+      end
     end
   end
 end
