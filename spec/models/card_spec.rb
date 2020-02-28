f9848655899f5a16a73334fbfe2ccd6fcde83a7d
atsuyakoshida <koshi0845@gmail.com>
2020-02-28 17:46:57 +0900

    add test

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 describe Card do
   describe '#pay' do
     it "is invalid without a card_id" do
+      card = build(:card, card_id: "")
+      card.valid?
+      expect(card.errors[:card_id]).to include("can't be blank")
     end
   end
 end
